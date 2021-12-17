315153312fb6e8d52d7140fa3d2132fb67284e21
Rafael Collado <rafaelcolladojr@gmail.com>
2021-12-17 01:04:15 -0500

    Removed weather api key from constants file.

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,4 +1,3 @@
 const String kCachedWeatherKey = 'CACHED_WEATHER';
 const String kWeatherApiBaseUrl =
     'https://api.openweathermap.org/data/2.5/weather?q=';
-const String kWeatherApiKey = '3d89e221bda0626e92370e0a74e0dc3a';
